9cf5c2d22b658fca18c5bab7915fc3b6928316eb
RemiJonathan <contact@remijonathan.com>
2019-11-19 23:15:07 -0500

    Made ANN

--- ann.py
+++ ann.py
@@ -1,4 +1,16 @@
-# Data Pre processing
+# Artificial Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Data Preprocessing
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,13 +26,10 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-
 onehotencoder = OneHotEncoder(categorical_features=[1])
 X = onehotencoder.fit_transform(X).toarray()
-
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
@@ -35,20 +44,39 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Data Processing Section
+# Part 2 - Now let's make the ANN!
 
-# Importing Keras library and packages
+# Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
-# Initialize the Artificial Nural Network
+# Initialising the ANN
+classifier = Sequential()
 
-# Fitting classifier to the Training set
-# Create your classifier here
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
+# classifier.add(Dropout(p = 0.1))
+
+# Adding the second hidden layer
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
+# classifier.add(Dropout(p = 0.1))
+
+# Adding the output layer
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+# Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
